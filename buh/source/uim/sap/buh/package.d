f8030e9762d24f84713052e204398fc4c7667af6
UIMSolutions <ui.manufaktur@gmail.com>
2026-02-15 08:02:06 +0100

    feat: Implement UIM SAP Business Hub-like Service (BUH) with API management, Kubernetes support, and in-memory storage

---
+++ buh/source/uim/sap/buh/package.d
@@ -0,0 +1,13 @@
+/**
+ * UIM SAP Business Hub-like Service (BUH)
+ */
+module uim.sap.buh;
+
+public import uim.sap.buh.config;
+public import uim.sap.buh.exceptions;
+public import uim.sap.buh.models;
+public import uim.sap.buh.store;
+public import uim.sap.buh.service;
+public import uim.sap.buh.server;
+
+enum UIM_SAP_BUH_VERSION = "1.0.0";
---
+++ buh/source/uim/sap/buh/store.d
@@ -0,0 +1,85 @@
+/**
+ * In-memory store for BUH resources
+ */
+module uim.sap.buh.store;
+
+import core.sync.mutex : Mutex;
+
+import uim.sap.buh.models;
+
+class BUHStore {
+    private BUHApi[string] _apis;
+    private BUHProduct[string] _products;
+    private BUHSubscription[string] _subscriptions;
+    private Mutex _lock;
+
+    this() {
+        _lock = new Mutex;
+    }
+
+    BUHApi createApi(BUHApi api) {
+        synchronized (_lock) {
+            _apis[api.id] = api;
+            return api;
+        }
+    }
+
+    BUHProduct createProduct(BUHProduct product) {
+        synchronized (_lock) {
+            _products[product.id] = product;
+            return product;
+        }
+    }
+
+    BUHSubscription createSubscription(BUHSubscription subscription) {
+        synchronized (_lock) {
+            _subscriptions[subscription.id] = subscription;
+            return subscription;
+        }
+    }
+
+    BUHApi[] listApis() {
+        BUHApi[] values;
+        synchronized (_lock) {
+            foreach (entry; _apis.byValue) {
+                values ~= entry;
+            }
+        }
+        return values;
+    }
+
+    BUHProduct[] listProducts() {
+        BUHProduct[] values;
+        synchronized (_lock) {
+            foreach (entry; _products.byValue) {
+                values ~= entry;
+            }
+        }
+        return values;
+    }
+
+    BUHSubscription[] listSubscriptions() {
+        BUHSubscription[] values;
+        synchronized (_lock) {
+            foreach (entry; _subscriptions.byValue) {
+                values ~= entry;
+            }
+        }
+        return values;
+    }
+
+    bool hasApi(string id) {
+        synchronized (_lock) {
+            return (id in _apis) !is null;
+        }
+    }
+
+    BUHApi getApi(string id) {
+        synchronized (_lock) {
+            if (auto ptr = id in _apis) {
+                return *ptr;
+            }
+        }
+        return BUHApi.init;
+    }
+}
