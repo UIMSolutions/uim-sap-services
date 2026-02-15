407f88420b1f39db1810695cf87c5460464c6d76
UIMSolutions <ui.manufaktur@gmail.com>
2026-02-15 07:59:05 +0100

    feat: Add UIM SAP CloudFoundry-like Service (CLF) with Kubernetes support, configuration, and API endpoints

---
+++ clf/source/uim/sap/clf/exceptions.d
@@ -0,0 +1,34 @@
+/**
+ * Exceptions for CLF service
+ */
+module uim.sap.clf.exceptions;
+
+class CLFException : Exception {
+    this(string msg, string file = __FILE__, size_t line = __LINE__, Throwable next = null) {
+        super(msg, file, line, next);
+    }
+}
+
+class CLFConfigurationException : CLFException {
+    this(string msg, string file = __FILE__, size_t line = __LINE__, Throwable next = null) {
+        super(msg, file, line, next);
+    }
+}
+
+class CLFValidationException : CLFException {
+    this(string msg, string file = __FILE__, size_t line = __LINE__, Throwable next = null) {
+        super(msg, file, line, next);
+    }
+}
+
+class CLFNotFoundException : CLFException {
+    this(string entityType, string id, string file = __FILE__, size_t line = __LINE__, Throwable next = null) {
+        super(entityType ~ " not found: " ~ id, file, line, next);
+    }
+}
+
+class CLFAuthorizationException : CLFException {
+    this(string msg = "Unauthorized", string file = __FILE__, size_t line = __LINE__, Throwable next = null) {
+        super(msg, file, line, next);
+    }
+}
---
+++ clf/source/uim/sap/clf/package.d
@@ -0,0 +1,13 @@
+/**
+ * UIM SAP CloudFoundry-like Service (CLF)
+ */
+module uim.sap.clf;
+
+public import uim.sap.clf.config;
+public import uim.sap.clf.exceptions;
+public import uim.sap.clf.models;
+public import uim.sap.clf.store;
+public import uim.sap.clf.service;
+public import uim.sap.clf.server;
+
+enum UIM_SAP_CLF_VERSION = "1.0.0";
