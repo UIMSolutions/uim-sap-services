4ee652445cbf2ffa8cb24a255c3f1f647ba0598d
UIMSolutions <ui.manufaktur@gmail.com>
2026-02-24 20:00:35 +0100

    Remove Site Manager service and related files
    - Deleted the README.md file containing documentation for the Site Manager service.
    - Removed the build script for the uim-sap-smg-service.
    - Eliminated the dub.sdl file which defined the project and its dependencies.
    - Cleared the dub.selections.json file that specified package versions.
    - Removed Kubernetes configuration files including configmap, deployment, and service YAMLs.
    - Deleted source files related to the Site Manager service, including app, config, exceptions, models, package, server, service, and store modules.

--- Site Manager/source/uim/sap/smg/package.d
+++
@@ -1,8 +0,0 @@
-module uim.sap.smg;
-
-public import uim.sap.smg.config;
-public import uim.sap.smg.models;
-public import uim.sap.smg.service;
-public import uim.sap.smg.server;
-
-enum UIM_SAP_SMG_VERSION = "1.0.0";
