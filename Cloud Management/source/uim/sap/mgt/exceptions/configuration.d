af5502683358e3ef496df9eb13290eb6da62822f
UIMSolutions <ui.manufaktur@gmail.com>
2026-02-25 17:52:47 +0100

    chore: Remove Cloud Management service files and configurations

--- Cloud Management/source/uim/sap/mgt/exceptions/configuration.d
+++
@@ -1,7 +0,0 @@
-module uim.sap.mgt.exceptions.configuration;
-
-class MGTConfigurationException : MGTException {
-    this(string msg) {
-        super(msg);
-    }
-}
--- Cloud Management/source/uim/sap/mgt/exceptions/exception.d
+++
@@ -1,7 +0,0 @@
-module uim.sap.mgt.exceptions.exception;
-
-class MGTException : Exception {
-    this(string msg) {
-        super(msg);
-    }
-}
