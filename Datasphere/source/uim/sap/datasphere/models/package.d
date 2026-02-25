18c3cc0feab76e95d5c8038f040704160a2bfdbc
UIMSolutions <ui.manufaktur@gmail.com>
2026-02-25 17:46:35 +0100

    Remove unused models and service files from Datasphere module
    - Deleted `space.d` model which represented a space in the Datasphere application.
    - Removed `tenantadminstate.d` model that tracked the state of tenant administration.
    - Eliminated `package.d` which contained module imports and versioning information.
    - Cleared out `server.d` which handled HTTP requests and responses for the Datasphere service.
    - Removed `service.d` that contained the core business logic and service methods for Datasphere.
    - Deleted `store.d` which managed data storage and retrieval for various entities in the Datasphere.

--- Datasphere/source/uim/sap/datasphere/models/package.d
+++
@@ -1,9 +0,0 @@
-module uim.sap.datasphere.models;
-
-public {
-    import uim.sap.datasphere.models.datamodel;
-    import uim.sap.datasphere.models.space;
-    import uim.sap.datasphere.models.businessmodel;
-    import uim.sap.datasphere.models.integrationconnection;
-    import uim.sap.datasphere.models.governanceasset;
-}
