4e3e10a2547681612ee42f3472ca4b497c2934ee
UIMSolutions <ui.manufaktur@gmail.com>
2026-02-26 06:45:54 +0100

    feat: add copyright and license information to multiple source files

--- site-manager/source/uim/sap/smg/exceptions/authorization.d
+++ site-manager/source/uim/sap/smg/exceptions/authorization.d
@@ -1,5 +1,28 @@
+/****************************************************************************************************************
+* Copyright: © 2018-2026 Ozan Nurettin Süel (aka UI-Manufaktur UG *R.I.P*) 
+* License: Subject to the terms of the Apache 2.0 license, as written in the included LICENSE.txt file. 
+* Authors: Ozan Nurettin Süel (aka UI-Manufaktur UG *R.I.P*)
+*****************************************************************************************************************/
 module uim.sap.smg.exceptions.authorization;
 
+import uim.sap.smg;
+
+mixin(ShowModule!());
+
+@safe:
+
+/** 
+    * Exception thrown when a user is not authorized to perform an action.
+    *
+    * This could be due to insufficient permissions, invalid credentials, or other authorization issues.
+    * 
+    * Example usage:
+    * try {
+    *     // Code that may throw an authorization exception
+    * } catch (SMGAuthorizationException ex) {
+    *     // Handle the authorization error
+    * }
+    */
 class SMGAuthorizationException : SMGException {
     this(string msg) {
         super(msg);
--- site-manager/source/uim/sap/smg/exceptions/configuration.d
+++ site-manager/source/uim/sap/smg/exceptions/configuration.d
@@ -1,5 +1,25 @@
+/****************************************************************************************************************
+* Copyright: © 2018-2026 Ozan Nurettin Süel (aka UI-Manufaktur UG *R.I.P*) 
+* License: Subject to the terms of the Apache 2.0 license, as written in the included LICENSE.txt file. 
+* Authors: Ozan Nurettin Süel (aka UI-Manufaktur UG *R.I.P*)
+*****************************************************************************************************************/
 module uim.sap.smg.exceptions.configuration;
 
+import uim.sap.smg;
+@safe:
+
+/** 
+    * Exception thrown when there is a configuration error in the Site Manager.
+    *
+    * This could be due to missing or invalid configuration values, or issues with loading configuration files.
+    *
+    * Example usage:
+    * try {
+    *     // Code that may throw a configuration exception
+    * } catch (SMGConfigurationException ex) {
+    *     // Handle the configuration error
+    * }
+    */
 class SMGConfigurationException : SMGException {
     this(string msg) {
         super(msg);
