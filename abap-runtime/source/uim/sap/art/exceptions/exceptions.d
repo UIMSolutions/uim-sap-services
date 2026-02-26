bf255f0c89266d626fb23147ce7430cebc0d157c
UIMSolutions <ui.manufaktur@gmail.com>
2026-02-26 06:48:57 +0100

    feat: add copyright and license information to multiple source files

--- abap-runtime/source/uim/sap/art/exceptions/exceptions.d
+++ abap-runtime/source/uim/sap/art/exceptions/exceptions.d
@@ -1,6 +1,8 @@
-/**
- * Exceptions for SAP ABAP Runtime (ART)
- */
+/****************************************************************************************************************
+* Copyright: © 2018-2026 Ozan Nurettin Süel (aka UI-Manufaktur UG *R.I.P*) 
+* License: Subject to the terms of the Apache 2.0 license, as written in the included LICENSE.txt file. 
+* Authors: Ozan Nurettin Süel (aka UI-Manufaktur UG *R.I.P*)
+*****************************************************************************************************************/
 module uim.sap.art.exceptions.exceptions;
 
 class ARTRuntimeException : Exception {
