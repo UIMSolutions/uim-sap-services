a143a71dbc46449392b76bcf586904fa641e6233
UIMSolutions <ui.manufaktur@gmail.com>
2026-02-28 18:33:32 +0100

    feat: Update copyright information and licensing in multiple files

--- application-autoscaler/source/uim/sap/aas/helpers/package.d
+++ application-autoscaler/source/uim/sap/aas/helpers/package.d
@@ -3,10 +3,5 @@
 * License: Subject to the terms of the Apache 2.0 license, as written in the included LICENSE.txt file. 
 * Authors: Ozan Nurettin Süel (aka UI-Manufaktur UG *R.I.P*)
 *****************************************************************************************************************/
-module uim.sap.art.exceptions.exception;
+module uim.sap.aas.helpers;
 
-class ARTRuntimeException : Exception {
-    this(string msg, string file = __FILE__, size_t line = __LINE__, Throwable next = null) {
-        super(msg, file, line, next);
-    }
-}
