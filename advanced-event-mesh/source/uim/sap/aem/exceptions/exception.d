30b9f4dba175c1e68f30f2e27c52fdaef944229e
UIMSolutions <ui.manufaktur@gmail.com>
2026-03-06 13:54:22 +0100

    feat: Add copyright and license information to source files

--- advanced-event-mesh/source/uim/sap/aem/exceptions/exception.d
+++ advanced-event-mesh/source/uim/sap/aem/exceptions/exception.d
@@ -1,3 +1,8 @@
+/****************************************************************************************************************
+* Copyright: © 2018-2026 Ozan Nurettin Süel (aka UI-Manufaktur UG *R.I.P*) 
+* License: Subject to the terms of the Apache 2.0 license, as written in the included LICENSE.txt file. 
+* Authors: Ozan Nurettin Süel (aka UI-Manufaktur UG *R.I.P*)
+*****************************************************************************************************************/
 module uim.sap.aem.exceptions.exception;
 
 import uim.sap.aem;
@@ -6,7 +11,7 @@ mixin(ShowModule!());
 
 @safe:
 
-class AEMException : Exception {
+class AEMException : SAPException {
   this(string message) {
     super(message);
   }
--- advanced-event-mesh/source/uim/sap/aem/models/subscription.d
+++ advanced-event-mesh/source/uim/sap/aem/models/subscription.d
@@ -1,3 +1,8 @@
+/****************************************************************************************************************
+* Copyright: © 2018-2026 Ozan Nurettin Süel (aka UI-Manufaktur UG *R.I.P*) 
+* License: Subject to the terms of the Apache 2.0 license, as written in the included LICENSE.txt file. 
+* Authors: Ozan Nurettin Süel (aka UI-Manufaktur UG *R.I.P*)
+*****************************************************************************************************************/
 module uim.sap.aem.models.subscription;
 
 import uim.sap.aem;
@@ -5,7 +10,6 @@ import uim.sap.aem;
 mixin(ShowModule!());
 
 @safe:
-
 
 struct AEMSubscription {
     string tenantId;
