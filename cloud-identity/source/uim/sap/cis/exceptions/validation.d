a0bef186b91b1af0a5fd6d88fa2a6e46d59302aa
UIMSolutions <ui.manufaktur@gmail.com>
2026-03-06 13:54:33 +0100

    feat: Add copyright and license information to multiple source files

--- cloud-identity/source/uim/sap/cis/exceptions/validation.d
+++ cloud-identity/source/uim/sap/cis/exceptions/validation.d
@@ -1,3 +1,8 @@
+/****************************************************************************************************************
+* Copyright: © 2018-2026 Ozan Nurettin Süel (aka UI-Manufaktur UG *R.I.P*) 
+* License: Subject to the terms of the Apache 2.0 license, as written in the included LICENSE.txt file. 
+* Authors: Ozan Nurettin Süel (aka UI-Manufaktur UG *R.I.P*)
+*****************************************************************************************************************/
 module uim.sap.cis.exceptions.validation;
 
 import uim.sap.cis;
@@ -7,7 +12,7 @@ mixin(ShowModule!());
 @safe:
 
 class CISValidationException : CISException {
-    this(string msg) {
-        super(msg);
-    }
+  this(string msg) {
+    super(msg);
+  }
 }
--- cloud-identity/source/uim/sap/cis/package.d
+++ cloud-identity/source/uim/sap/cis/package.d
@@ -1,16 +1,21 @@
+/****************************************************************************************************************
+* Copyright: © 2018-2026 Ozan Nurettin Süel (aka UI-Manufaktur UG *R.I.P*) 
+* License: Subject to the terms of the Apache 2.0 license, as written in the included LICENSE.txt file. 
+* Authors: Ozan Nurettin Süel (aka UI-Manufaktur UG *R.I.P*)
+*****************************************************************************************************************/
 module uim.sap.cis;
 
 public {
-    import uim.sap.service;
+  import uim.sap.service;
 
-    import uim.sap.cis.config;
-    import uim.sap.cis.enumerations;
-    import uim.sap.cis.exceptions;
-    import uim.sap.cis.helpers;
-    import uim.sap.cis.models;
-    import uim.sap.cis.store;
-    import uim.sap.cis.service;
-    import uim.sap.cis.server;
+  import uim.sap.cis.config;
+  import uim.sap.cis.enumerations;
+  import uim.sap.cis.exceptions;
+  import uim.sap.cis.helpers;
+  import uim.sap.cis.models;
+  import uim.sap.cis.store;
+  import uim.sap.cis.service;
+  import uim.sap.cis.server;
 }
 
 enum UIM_CIS_VERSION = "1.0.0";
