2ae9f66a060af3020e178c5c831f047d7c101ce6
UIMSolutions <ui.manufaktur@gmail.com>
2026-03-10 11:57:04 +0100

    feat: Add copyright and license information to multiple source files

--- object-store/source/uim/sap/obs/config.d
+++ object-store/source/uim/sap/obs/config.d
@@ -1,4 +1,9 @@
-module uim.sap.obs.config;
+/****************************************************************************************************************
+* Copyright: © 2018-2026 Ozan Nurettin Süel (aka UI-Manufaktur UG *R.I.P*) 
+* License: Subject to the terms of the Apache 2.0 license, as written in the included LICENSE.txt file. 
+* Authors: Ozan Nurettin Süel (aka UI-Manufaktur UG *R.I.P*)
+*****************************************************************************************************************/
+modulemodule uim.sap.obs.config;
 
 import uim.sap.obs;
 
--- object-store/source/uim/sap/obs/models/package.d
+++ object-store/source/uim/sap/obs/models/package.d
@@ -1,8 +1,15 @@
+/****************************************************************************************************************
+* Copyright: © 2018-2026 Ozan Nurettin Süel (aka UI-Manufaktur UG *R.I.P*) 
+* License: Subject to the terms of the Apache 2.0 license, as written in the included LICENSE.txt file. 
+* Authors: Ozan Nurettin Süel (aka UI-Manufaktur UG *R.I.P*)
+*****************************************************************************************************************/
 module uim.sap.obs.models;
 
-public import uim.sap.obs.models.bucket;
-public import uim.sap.obs.models.object_;
-public import uim.sap.obs.models.credential;
-public import uim.sap.obs.models.multipart;
-public import uim.sap.obs.models.lifecycle;
-public import uim.sap.obs.models.replication;
+public {
+  import uim.sap.obs.models.bucket;
+  import uim.sap.obs.models.object_;
+  import uim.sap.obs.models.credential;
+  import uim.sap.obs.models.multipart;
+  import uim.sap.obs.models.lifecycle;
+  import uim.sap.obs.models.replication;
+}
