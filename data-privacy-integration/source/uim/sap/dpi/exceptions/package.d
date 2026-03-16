727cc8c0b555349938e44aa5014439a9865e2771
UIMSolutions <ui.manufaktur@gmail.com>
2026-03-16 17:43:34 +0100

    refactor: add copyright and license information to multiple files

--- data-privacy-integration/source/uim/sap/dpi/exceptions/package.d
+++ data-privacy-integration/source/uim/sap/dpi/exceptions/package.d
@@ -1,8 +1,11 @@
+/****************************************************************************************************************
+* Copyright: © 2018-2026 Ozan Nurettin Süel (aka UI-Manufaktur UG *R.I.P*) 
+* License: Subject to the terms of the Apache 2.0 license, as written in the included LICENSE.txt file. 
+* Authors: Ozan Nurettin Süel (aka UI-Manufaktur UG *R.I.P*)
+*****************************************************************************************************************/
 module uim.sap.dpi.exceptions;
 
 public {
-  import uim.framework.exceptions;
-  
   import uim.sap.dpi.exceptions.authorization;
   import uim.sap.dpi.exceptions.configuration;
   import uim.sap.dpi.exceptions.exception;
--- data-privacy-integration/source/uim/sap/dpi/package.d
+++ data-privacy-integration/source/uim/sap/dpi/package.d
@@ -1,6 +1,13 @@
+/****************************************************************************************************************
+* Copyright: © 2018-2026 Ozan Nurettin Süel (aka UI-Manufaktur UG *R.I.P*) 
+* License: Subject to the terms of the Apache 2.0 license, as written in the included LICENSE.txt file. 
+* Authors: Ozan Nurettin Süel (aka UI-Manufaktur UG *R.I.P*)
+*****************************************************************************************************************/
 module uim.sap.dpi;
 
 public {
+  import uim.sap.service;
+  
   import uim.sap.dpi.config;
   import uim.sap.dpi.exceptions;
   import uim.sap.dpi.models;
