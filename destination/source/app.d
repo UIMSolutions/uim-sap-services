d2ede84809c955cbec88c8be945dac2a9f6da63c
UIMSolutions <ui.manufaktur@gmail.com>
2026-03-17 12:36:59 +0100

    refactor: add copyright and license information to multiple files

--- destination/source/app.d
+++ destination/source/app.d
@@ -1,10 +1,15 @@
+/****************************************************************************************************************
+* Copyright: © 2018-2026 Ozan Nurettin Süel (aka UI-Manufaktur UG *R.I.P*) 
+* License: Subject to the terms of the Apache 2.0 license, as written in the included LICENSE.txt file. 
+* Authors: Ozan Nurettin Süel (aka UI-Manufaktur UG *R.I.P*)
+*****************************************************************************************************************/
 module app;
 
-import std.conv : to;
-import std.process : environment;
-import std.stdio : writeln;
-
 import uim.sap.dst;
+
+mixin(ShowModule!());
+
+@safe:
 
 void main() {
   DSTConfig config = new DSTConfig();
--- destination/source/uim/sap/dst/exceptions/destination.d
+++ destination/source/uim/sap/dst/exceptions/destination.d
@@ -1,4 +1,10 @@
+/****************************************************************************************************************
+* Copyright: © 2018-2026 Ozan Nurettin Süel (aka UI-Manufaktur UG *R.I.P*) 
+* License: Subject to the terms of the Apache 2.0 license, as written in the included LICENSE.txt file. 
+* Authors: Ozan Nurettin Süel (aka UI-Manufaktur UG *R.I.P*)
+*****************************************************************************************************************/
 module uim.sap.dst.exceptions.destination;
+
 import uim.sap.dst;
 
 mixin(ShowModule!());
