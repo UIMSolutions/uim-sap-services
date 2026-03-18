a300c13dfaa5bd110db1d14f7901092572c46e67
Ozan Nurettin Süel <ui.manufaktur@gmail.com>
2026-03-18 11:28:25 +0100

    Aktualisieren von system.d

--- cloud-integration-automation/source/uim/sap/cia/models/system.d
+++ cloud-integration-automation/source/uim/sap/cia/models/system.d
@@ -7,29 +7,24 @@ mixin(ShowModule!());
 // ---------------------------------------------------------------------------
 // System – an entry in the system landscape (SAP Cloud, On-Premises, …)
 // ---------------------------------------------------------------------------
-struct CIASystem {
-  string tenantId;
-  string id;
+class CIASystem : SAPTenantObject {
+mixin(SAPObjectTemplate!CIASystem);
+
+  UUID id;
   string name;
   /// System type: "sap-cloud", "s4hana-on-prem", "s4hana-cloud", "successfactors", "ariba", "other"
   string systemType;
   string host;
   string description;
   bool active;
-  SysTime createdAt;
-  SysTime updatedAt;
-
+  
   override Json toJson()  {
-    Json j = Json.emptyObject;
-    j["tenant_id"] = tenantId;
-    j["id"] = id;
-    j["name"] = name;
-    j["system_type"] = systemType;
-    j["host"] = host;
-    j["description"] = description;
-    j["active"] = active;
-    j["created_at"] = createdAt.toISOExtString();
-    j["updated_at"] = updatedAt.toISOExtString();
-    return j;
-  }
+    return super.toJson
+    .set("id", id)
+    .set("name", name)
+    .set("system_type", systemType)
+    .set("host", host)
+    .set("description", description)
+    .set("active"] = active);
+   }
 }
